aa9b7b9089a310ba60dfc92c33688195b48b8b38
Krishna Kothapalli <krishna_sunitha@hotmail.com>
2014-11-26 13:58:26 -0800

    Client test case for Policy Target Group
    Partial-Bug: 1393865
    Change-Id: I858cd5f8f2de27bfd44665817fbfa064a9baf46c

---
+++ gbpclient/tests/unit/test_cli20_policytargetgroup.py
@@ -0,0 +1,41 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+import logging
+import sys
+
+from gbpclient.gbp.v2_0 import groupbasedpolicy as gbp
+from gbpclient.tests.unit import test_cli20
+
+
+class CLITestV20PolicyTargetGroupJSON(test_cli20.CLITestV20Base):
+
+    LOG = logging.getLogger(__name__)
+
+    def setUp(self):
+        super(CLITestV20PolicyTargetGroupJSON, self).setUp()
+
+    def test_create_policy_target_group_with_mandatory_params(self):
+        """policy-target-group-create with all mandatory params."""
+        resource = 'policy_target_group'
+        cmd = gbp.CreatePolicyTargetGroup(test_cli20.MyApp(sys.stdout), None)
+        name = 'my-name'
+        tenant_id = 'my-tenant'
+        my_id = 'my-id'
+        args = ['--tenant-id', tenant_id,
+                name]
+        position_names = ['name', ]
+        position_values = [name, ]
+        self._test_create_resource(resource, cmd, name, my_id, args,
+                                   position_names, position_values,
+                                   tenant_id=tenant_id)
