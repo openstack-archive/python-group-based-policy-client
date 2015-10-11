b8e80eaf86d733d678d2f592184708398c7666f5
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2015-10-11 01:49:30 -0700

    Friendlier option for providing dict key arguments
    Several CLI commands require providing dictionary arguments. These
    arguments are currently provided as:
    
    --argument-name "key1=<value1,key2=value2,...>"
    
    Although the value fields are optional, it requires the user to
    at least provide input in the form "key=". This is not very intuitive
    and often confusing.
    
    In this patch we preseve the current convention of providing the
    dictionary arguments, but we enhance the parsing logic to also accept only
    keys as arguments. The values are assumed as empty strings in such cases
    and the appropriate dictonary is constructed and passed to the backend. So,
    the following new syntax is also supported:
    
    --argument-name "key1<,key2,...>"
    
    This is a backward compatible change to the CLI.
    
    Change-Id: I0f4c15b02c151a5f4facb2881fb0c11896a7e39c
    Closes-bug: 1504912
    (cherry picked from commit df9540094af38d5eb4900fbd507fd460b60e122b)
    (cherry picked from commit ff513dec94a6672e8e1d7ce29f733eace02db9ac)

---
+++ gbpclient/tests/unit/test_utils.py
@@ -0,0 +1,39 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import testtools
+
+from gbpclient.common import utils
+
+
+class TestUtils(testtools.TestCase):
+    def test_string_to_dictionary(self):
+        input_str = 'key1'
+        expected = {'key1': ''}
+        self.assertEqual(expected, utils.str2dict(input_str))
+        input_str = 'key1,key2'
+        expected = {'key1': '', 'key2': ''}
+        self.assertEqual(expected, utils.str2dict(input_str))
+        input_str = 'key1=value1,key2'
+        expected = {'key1': 'value1', 'key2': ''}
+        self.assertEqual(expected, utils.str2dict(input_str))
+        input_str = 'key1=value1,key2=value2'
+        expected = {'key1': 'value1', 'key2': 'value2'}
+        self.assertEqual(expected, utils.str2dict(input_str))
+
+    def test_none_string_to_dictionary(self):
+        input_str = ''
+        expected = {}
+        self.assertEqual(expected, utils.str2dict(input_str))
+        input_str = None
+        expected = {}
+        self.assertEqual(expected, utils.str2dict(input_str))
