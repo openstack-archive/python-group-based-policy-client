e42fc24824d48e4e52a5b8b29146d4a6d4a27d9a
Steve Martinelli <stevemar@ca.ibm.com>
2015-12-27 01:48:34 -0500

    use keystoneclient exceptions instead of oslo-incubator code
    depending on any oslo-incubator code from another project is
    dangerous. keystoneclient makes its exceptions public and it's
    not recommended to use any code from
    keystoneclient.openstack.common.apiclient since it's maintained
    by oslo-incubator.
    
    Change-Id: I3541f0a20543d33b059ace5110020ec5d22e182f

--- gbpclient/gbpshell.py
+++ gbpclient/gbpshell.py
@@ -25,7 +25,7 @@ import sys
 from keystoneclient.auth.identity import v2 as v2_auth
 from keystoneclient.auth.identity import v3 as v3_auth
 from keystoneclient import discover
-from keystoneclient.openstack.common.apiclient import exceptions as ks_exc
+from keystoneclient import exceptions as ks_exc
 from keystoneclient import session
 from oslo.utils import encodeutils
 import six.moves.urllib.parse as urlparse
